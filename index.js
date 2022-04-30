d15e096db131696233f4b3ebf7df999bf297dd00
minhquach <minhquach8@gmail.com>
2022-04-30 22:09:52 +0700

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/homepage", (req, res) => res.send("Hello World!"));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
