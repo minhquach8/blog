69b1af4211a1ebde199b8dd4f44224d7a1b59aed
minhquach <minhquach8@gmail.com>
2022-05-01 19:38:46 +0700

    prettier, lint-staged, husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const newsRouter = require("./newsRoute");
-const siteRouter = require("./siteRoute");
+const newsRouter = require('./newsRoute');
+const siteRouter = require('./siteRoute');
 
 function route(app) {
-	app.use("/news", newsRouter);
-	app.use("/", siteRouter);
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
 }
 
 module.exports = route;
